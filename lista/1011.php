54abce99f054dcb53ab6d3eb5a4a155b6025eec9
annabortoleto <annabortoleto@gmail.com>
2024-02-25 21:24:04 -0300

    prw2021

--- lista/1011.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$numFuncionario = intval (readline());
-$horasTrabalhadas = intval (readline());
-$valorHora = floatval (readline());
-
-echo "NUMBER = $numFuncionario\nSALARY = U$ " . number_format ($horasTrabalhadas * $valorHora, 2, ".", "") . "\n";
-
-?>
