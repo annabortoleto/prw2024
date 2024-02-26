25efbc9308ed98a5c47ca59171b8bb4a2cf3b9b6
annabortoleto <annabortoleto@gmail.com>
2024-02-25 21:08:51 -0300

    prw2024

---
+++ lista/1007.php
@@ -0,0 +1,11 @@
+<?php
+
+$x = intval(readline());
+$y = intval(readline());
+$z = intval(readline());
+$w = intval(readline());
+
+$diferenca = ($x * $y - $z * $w);
+echo "DIFERENCA = $diferenca\n";
+
+?>
