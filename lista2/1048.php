6a56254ab4919aa482db58b717a3920c25db3c5e
annabortoleto <annabortoleto@gmail.com>
2024-03-12 09:58:51 -0300

    prw2024

---
+++ lista2/1048.php
@@ -0,0 +1,26 @@
+<?php
+
+$salario = floatval(readline());
+
+$novo = $reajuste = $percentual = 0;
+
+if ($salario <= 400.00) {
+    $percentual = 15;
+} elseif ($salario <= 800.00) {
+    $percentual = 12;
+} elseif ($salario <= 1200.00) {
+    $percentual = 10;
+} elseif ($salario <= 2000.00) {
+    $percentual = 7;
+} else {
+    $percentual = 4;
+}
+
+$reajuste = $salario * ($percentual / 100);
+$novo = $salario + $reajuste;
+
+echo "Novo salario: " . number_format($novo, 2, '.', '') . "\n";
+echo "Reajuste ganho: " . number_format($reajuste, 2, '.', '') . "\n";
+echo "Em percentual: " . $percentual . " %\n";
+
+?>
