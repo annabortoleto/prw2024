e75f21f3db2d5d27efee0483f71d4cbfafee564e
annabortoleto <annabortoleto@gmail.com>
2024-03-12 09:32:55 -0300

    pwr2024

--- lista2/1043.php
+++ lista2/1043.php
@@ -1,14 +1,12 @@
 <?php
 
-list($x, $y, $z) = array_map('floatval', explode(' ', readline()));
+list($a, $b, $c) = array_map('floatval', explode(' ', readline()));
 
-
-if (($x + $y > $z) && ($x + $z > $y) && ($y + $z > $a)) {
+if (($a + $b > $c) && ($a + $c > $b) && ($b + $c > $a)) {
     $perimetro = $x + $y + $z;
-    
-echo "Perimetro = " . number_format($perimetro, 1, '.', '') . "\n";
+    echo "Perimetro = " . number_format($perimetro, 1, '.', '') . "\n";
 } else {
-    $area = (($x + $y) * $z)/2;
+    $area = (($a + $b) * $c)/2;
     echo "Area = " . number_format($area, 1, '.', '') . "\n";
 }
 ?>
