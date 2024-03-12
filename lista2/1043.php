365e0e71196b7f2203591838a86765f9b83c9d24
annabortoleto <annabortoleto@gmail.com>
2024-03-12 09:40:23 -0300

    prw2024

--- lista2/1043.php
+++ lista2/1043.php
@@ -3,10 +3,10 @@
 list($a, $b, $c) = array_map('floatval', explode(' ', readline()));
 
 if (($a + $b > $c) && ($a + $c > $b) && ($b + $c > $a)) {
-    $perimetro = $x + $y + $z;
+    $perimetro = $a + $b + $c;
     echo "Perimetro = " . number_format($perimetro, 1, '.', '') . "\n";
 } else {
-    $area = (($a + $b) * $c)/2;
+    $area = (($a + $b) * $c) / 2;
     echo "Area = " . number_format($area, 1, '.', '') . "\n";
 }
 ?>
