1778330cf08f7eb9b36cd25f7f7d0c6b82511bbd
annabortoleto <annabortoleto@gmail.com>
2024-03-12 09:46:59 -0300

    prw2024

--- lista2/1047.php
+++ lista2/1047.php
@@ -2,13 +2,14 @@
 
 list($inicioHora, $inicioMinuto, $fimHora, $fimMinuto) = array_map('intval', explode(' ', readline()));
 
-$inicialMinutos = $inicioHora * 60 + $minutoInicial;
+$inicialMinutos = $inicioHora * 60 + $inicioMinuto;
 $finalMinutos = $fimHora * 60 + $fimMinuto;
 
-$duracaoMinutos = ($finalMinutos - $inicialMinutos + 24 * 60) % (24 * 60);
+if ($finalMinutos <= $inicialMinutos) {
+    $finalMinutos += 24 * 60;
+}
+$duracaoMinutos = $finalMinutos - $inicialMinutos;
 $duracaoHoras = intdiv($duracaoMinutos, 60);
 $duracaoMinutos %= 60;
 
 echo "O JOGO DUROU " . $duracaoHoras . " HORA(S) E " . $duracaoMinutos . " MINUTO(S)\n";
-
-?>
